22f53e68e15bb64a4732161fe2decf5d4a25ad65
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2024-09-04 20:09:39 +0600

    feat: add spinner

---
+++ components/spinner/Spinner.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Lottie from 'react-lottie';
+import animationData from '@/public/gifs/loading.json';
+
+export default function Spinner({ animation = animationData }) {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animation,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+        },
+    };
+
+    return (
+        <div className="h-full w-full grid place-content-center">
+            <Lottie options={defaultOptions} height={140} width={140} />
+        </div>
+    );
+}
